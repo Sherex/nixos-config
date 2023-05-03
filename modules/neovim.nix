26ead473100e16880491ed96bcae3647e55fed12
Ingar Helgesen <ingar@i-h.no>
2023-05-03 02:01:50 +0200

    add treesitter + langs

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -15,6 +15,30 @@
           plugin = pkgs.vimPlugins.vim-startify;
           config = "let g:startify_change_to_vcs_root = 0";
 	}
+        (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+	  # https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+	  p.lua
+	  p.bash
+	  p.c_sharp
+	  p.comment
+	  p.dockerfile
+	  p.git_config
+	  p.git_rebase
+	  p.gitattributes
+	  p.gitcommit
+	  p.gitignore
+	  p.html
+	  p.css
+	  p.ini
+	  p.javascript
+	  p.json
+	  p.nix
+	  p.regex
+	  p.terraform
+	  p.toml
+	  p.typescript
+	  p.yaml
+	]))
       ];
     };
   };
