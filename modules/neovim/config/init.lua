7fc4a16dff8e1f7d70d6b9138cbcecefca8ed29d
Ingar Helgesen <ingar@i-h.no>
2023-05-08 03:17:09 +0200

    refactor don't use Nix for Neovim config

---
+++ modules/neovim/config/init.lua
@@ -0,0 +1,76 @@
+-- Install Lazy.nvim if not already installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Disable netrw for nvim-tree (has to be at the top of the file)
+-- https://github.com/nvim-tree/nvim-tree.lua#quick-start
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Set termguicolors to enable highlight groups
+-- https://github.com/nvim-tree/nvim-tree.lua#quick-start
+-- :h termguicolors
+vim.opt.termguicolors = true
+
+-- Enable line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- TODO: Move to own modules
+require("lazy").setup({
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>e"] = { name = "+NvimTree" },
+      })
+    end,
+  },
+
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+
+  "folke/neodev.nvim",
+
+  "folke/trouble.nvim",
+
+  { 
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+      { "<leader>ee", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+      { "<leader>ef", "<cmd>NvimTreeFindFile<cr>", desc = "Find file in NvimTree" },
+    },
+    config = true,
+  },
+
+  {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
+})
+
+-- Set colorscheme
+vim.cmd.colorscheme("tokyonight")
+
--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -4,76 +4,19 @@
   imports = [ <home-manager/nixos> ];
 
   home-manager.users.sherex = { pkgs, ... }: {
+   home.file."./.config/nvim/" = {
+     source = ./config;
+     recursive = true;
+   };
+
     programs.neovim = {
       enable = true;
       defaultEditor = true;
       vimAlias = true;
       viAlias = true;
-      extraLuaConfig = ''
-        -- Disable netrw for nvim-tree (has to be at the top of the file)
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-
-        -- Enable line numbering
-        vim.opt.number = true
-        vim.opt.relativenumber = true
-
-        -- Set leader key
-        vim.g.mapleader = ' '
-        vim.g.maplocalleader = ' '
-      '';
-      extraPackages = [
-        pkgs.gcc
-      ];
-      plugins = with pkgs.vimPlugins; [
-        plenary-nvim
-	{
-	  plugin = nvim-tree-lua;
-	  config = ''
-            packadd! nvim-tree.lua
-            lua vim.opt.termguicolors = true
-            lua require("nvim-tree").setup()
-	  '';
-	}
-	{
-          plugin = vim-startify;
-          config = "let g:startify_change_to_vcs_root = 0";
-	}
-        (nvim-treesitter.withPlugins (p: [
-	  # https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-	  p.lua
-	  p.bash
-	  p.c_sharp
-	  p.comment
-	  p.dockerfile
-	  p.git_config
-	  p.git_rebase
-	  p.gitattributes
-	  p.gitcommit
-	  p.gitignore
-	  p.html
-	  p.css
-	  p.ini
-	  p.javascript
-	  p.json
-	  p.nix
-	  p.regex
-	  p.terraform
-	  p.toml
-	  p.typescript
-	  p.yaml
-	]))
-	nvim-web-devicons # https://github.com/nvim-tree/nvim-web-devicons
-	telescope-fzf-native-nvim # https://github.com/nvim-telescope/telescope-fzf-native.nvim
-	telescope-nvim
-	{
-	  plugin = which-key-nvim;
-          config = ''
-            packadd! which-key.nvim
-            lua require("which-key").setup()
-	  '';
-	}
-      ];
+#      extraPackages = [
+#        pkgs.gcc
+#      ];
     };
   };
 }
