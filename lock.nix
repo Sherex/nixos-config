9e629bfb4abc9ee2dcd3406ece7debe0aaf1db99
Ingar Helgesen <ingar@i-h.no>
2023-07-14 03:58:29 +0200

    refactor lock.nix

--- lock.nix
+++ lock.nix
@@ -5,10 +5,11 @@ let
     nixos =         { ref = "f2406198ea0e4e37d4380d0e20336c575b8f8ef9"; repo = "NixOS/nixpkgs"; };
     home-manager =  { ref = "e42fb59768f0305085abde0dd27ab5e0cc15420c"; repo = "nix-community/home-manager"; };
   };
+  createGitHubArchiveUrl = { repo, ref }: "https://github.com/${repo}/archive/${ref}.tar.gz";
 in {
   repositories = builtins.mapAttrs (name: channel:
     if builtins.hasAttr "repo" channel
-    then "https://github.com/${channel.repo}/archive/${channel.ref}.tar.gz"
+    then createGitHubArchiveUrl channel
     else channel.ref
   ) channels;
 }
