8ed387648feff22c0b446b78abafe93e10595cd0
Ingar Helgesen <ingar@i-h.no>
2023-07-14 04:28:37 +0200

    update checksums for nixpkgs and nixos

--- lock.nix
+++ lock.nix
@@ -1,8 +1,8 @@
 let
   channels = {
     nixos-config =  { ref = "/etc/nixos/configuration.nix"; };
-    nixpkgs =       { ref = "bf57c599729771cd23054a18c0f3a391ae85e193"; repo = "NixOS/nixpkgs"; };
-    nixos =         { ref = "f2406198ea0e4e37d4380d0e20336c575b8f8ef9"; repo = "NixOS/nixpkgs"; };
+    nixpkgs =       { ref = "df1eee2aa65052a18121ed4971081576b25d6b5c"; repo = "NixOS/nixpkgs"; };
+    nixos =         { ref = "2de8efefb6ce7f5e4e75bdf57376a96555986841"; repo = "NixOS/nixpkgs"; };
     home-manager =  { ref = "e42fb59768f0305085abde0dd27ab5e0cc15420c"; repo = "nix-community/home-manager"; };
   };
   createGitHubArchiveUrl = { repo, ref }: "https://github.com/${repo}/archive/${ref}.tar.gz";
