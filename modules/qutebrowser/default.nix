86234b1465ebfd943132d138dc359da0978dea66
Ingar Helgesen <ingar@i-h.no>
2023-07-20 03:18:26 +0200

    refactor move qutebrowser to it's own module

---
+++ modules/qutebrowser/default.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, lib, home-manager, ... }:
+
+{
+  imports = [ home-manager.nixosModule ];
+
+  home-manager.users.sherex = { pkgs, ... }: {
+    programs.bash.sessionVariables.BROWSER = "qutebrowser";
+    programs.qutebrowser = {
+      enable = true;
+      loadAutoconfig = true;
+      aliases = { };
+      keyBindings = {
+        normal = {
+          "<Ctrl+e>" = "edit-url";
+          "D" = "tab-close -n";
+          "E" = ":edit-text";
+          "stf" = "spawn firefox {url}";
+          "stm" = "spawn kdeconnec-cli -n 'Pixel 7' --share {url}";
+          "stp" = "spawn mpv {url}";
+          "<F1>" = lib.mkMerge [
+            "config-cycle tabs.show never always"
+            "config-cycle statusbar.show in-mode always"
+            "config-cycle scrolling.bar never always"
+          ];
+        };
+      };
+      settings = {
+        auto_save.session = true;
+        colors = {
+          webpage = {
+            darkmode.enabled = true;
+            preferred_color_scheme = "dark";
+          };
+        };
+        content = {
+          autoplay = true;
+          javascript.can_access_clipboard = true;
+          pdfjs = false;
+        };
+        # TODO: Insert correct terminal and editor commands based on current environment
+        editor.command = [ "foot" "nvim" "-f" "{file}" "-c" "normal {line}G{column0}l" ];
+        tabs = {
+          background = false;
+          new_position.unrelated = "next";
+          position = "top";
+          select_on_remove = "last-used";
+          show = "always";
+          show_switching_delay = 800;
+        };
+      };
+    };
+  };
+}
+
--- sherex.nix
+++ sherex.nix
@@ -6,6 +6,7 @@
     ./modules/bash
     ./modules/neovim
     ./modules/foot
+    ./modules/qutebrowser
   ];
 
   # Install packages to /etc/profiles instead of ~/.nix-profile
@@ -28,49 +29,6 @@
       # TODO: Use variables for name and email
       userName = "Ingar Helgesen";
       userEmail = "ingar@i-h.no";
-    };
-    programs.qutebrowser = {
-      enable = true;
-      loadAutoconfig = true;
-      aliases = { };
-      keyBindings = {
-        normal = {
-          "<Ctrl+e>" = "edit-url";
-          "D" = "tab-close -n";
-          "E" = ":edit-text";
-          "stf" = "spawn firefox {url}";
-          "stm" = "spawn kdeconnec-cli -n 'Pixel 7' --share {url}";
-          "stp" = "spawn mpv {url}";
-          "<F1>" = lib.mkMerge [
-            "config-cycle tabs.show never always"
-            "config-cycle statusbar.show in-mode always"
-            "config-cycle scrolling.bar never always"
-          ];
-        };
-      };
-      settings = {
-        auto_save.session = true;
-        colors = {
-          webpage = {
-            darkmode.enabled = true;
-            preferred_color_scheme = "dark";
-          };
-        };
-        content = {
-          autoplay = true;
-          javascript.can_access_clipboard = true;
-          pdfjs = false;
-        };
-        editor.command = [ "kitty" "nvim" "-f" "'{file}'" "-c" "normal {line}G{column0}l" ];
-        tabs = {
-          background = false;
-          new_position.unrelated = "next";
-          position = "top";
-          select_on_remove = "last-used";
-          show = "always";
-          show_switching_delay = 800;
-        };
-      };
     };
     programs.vscode = {
       enable = true;
