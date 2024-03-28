31eb54db79af23ae02fab0a3c08130e8ab6d6ac7
Ingar Helgesen <ingar@i-h.no>
2024-03-29 00:19:31 +0100

    Remove i3status-rust package from Sway config

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -57,7 +57,6 @@ in
     slurp # screenshot functionality
     wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     rofi-wayland # wayland clone of dmenu
-    i3status-rust
     gammastep # Used by a block in i3status-rust
     complete-alias
     btop # Top alt.
