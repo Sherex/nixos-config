c3424877172ec36462a5511a1807b22c465837d4
Ingar Helgesen <ingar@i-h.no>
2024-03-29 17:22:54 +0100

    Replace Mini.Map with nvim-scrollbar

--- modules/neovim/config/init.lua
+++ modules/neovim/config/init.lua
@@ -459,46 +459,36 @@ require("lazy").setup({
     }
   },
 
+  { "kevinhwang91/nvim-hlslens" },
+
   {
-    "echasnovski/mini.map",
-    config = function ()
-      local map = require('mini.map')
-      map.setup({
-        integrations = {
-          map.gen_integration.builtin_search(),
-          map.gen_integration.gitsigns(),
-          map.gen_integration.diagnostic(),
-        },
-
-        symbols = {
-          encode = nil,
-          scroll_line = '█',
-          scroll_view = '┃',
-        },
-
-        window = {
-          focusable = false,
-          side = 'right',
-          show_integration_count = true,
-          width = 10,
-          winblend = 25,
-          zindex = 10,
-        },
-      })
-    end,
-    keys = {
-      {
-        "<leader>mm", desc = "Toggle MiniMap window", function ()
-          MiniMap.toggle()
-        end
-      },
-      {
-        "<leader>mf", desc = "Toggle MiniMap focus", function ()
-          MiniMap.open()
-          MiniMap.toggle_focus()
-        end
-      },
+    "petertriho/nvim-scrollbar",
+    dependencies = {
+      "kevinhwang91/nvim-hlslens",
+      "lewis6991/gitsigns.nvim",
+      "folke/tokyonight.nvim",
     },
+    lazy = false,
+    config = function ()
+      local colors = require("tokyonight.colors").setup()
+      local opts = {
+        handle = {
+          color = colors.bg_highlight,
+        },
+        marks = {
+          Search = { color = colors.orange },
+          Error = { color = colors.error },
+          Warn = { color = colors.warning },
+          Info = { color = colors.info },
+          Hint = { color = colors.hint },
+          Misc = { color = colors.purple },
+        },
+      }
+
+      require("scrollbar").setup(opts)
+      require("scrollbar.handlers.gitsigns").setup()
+      require("scrollbar.handlers.search").setup()
+    end
   },
 
 })
