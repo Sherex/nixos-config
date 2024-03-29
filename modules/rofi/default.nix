24d388c0d08cdb8a4a7f3440099cf38750ed7a52
Ingar Helgesen <ingar@i-h.no>
2024-03-29 11:02:52 +0100

    Move rofi themes

--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -9,7 +9,7 @@
       enable = true;
       font = "hack 10";
       terminal = "${config.home-manager.users.sherex.programs.bash.sessionVariables.TERMINAL}";
-      theme = toString ./themes/iceberg-dark.rasi;
+      theme = toString ./themes/default/iceberg-dark.rasi;
       extraConfig = {
         modi = "combi";
         show-icons = true;
