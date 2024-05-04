f102d9a9147830995af7039e67a918866b7aecd4
Ingar Helgesen <ingar@i-h.no>
2024-05-04 15:01:43 +0200

    Extract nixos system definition to function

--- flake.nix
+++ flake.nix
@@ -15,28 +15,23 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, impermanence, sops-nix }@attrs: {
-    nixosConfigurations.NixTop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs;
-      modules = [
-        ./systems/nixtop/configuration.nix
-        # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
-        # Source: https://ayats.org/blog/channels-to-flakes/
-        { nix.registry.nixpkgs.flake = nixpkgs; }
-      ];
-    };
+  outputs = { self, nixpkgs, home-manager, impermanence, sops-nix }@attrs:
+  let
+    nixosSystem = nixpkgs: attrs: name:
+      nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = attrs;
+        modules = [
+          ./systems/${name}/configuration.nix
+          # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
+          # Source: https://ayats.org/blog/channels-to-flakes/
+          { nix.registry.nixpkgs.flake = nixpkgs; }
+        ];
+      };
+  in {
+    nixosConfigurations.NixTop = nixosSystem nixpkgs attrs "nixtop";
 
-    nixosConfigurations.Archy = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs;
-      modules = [
-        ./systems/archy/configuration.nix
-        # This fixes nixpkgs (for e.g. "nix shell") to match the system nixpkgs
-        # Source: https://ayats.org/blog/channels-to-flakes/
-        { nix.registry.nixpkgs.flake = nixpkgs; }
-      ];
-    };
+    nixosConfigurations.Archy = nixosSystem nixpkgs attrs "archy";
 
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
