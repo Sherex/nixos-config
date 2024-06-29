1be9ae83faa4e2735dc112f9d35851830cbf8ec9
Ingar Helgesen <ingar@i-h.no>
2024-06-29 16:53:46 +0200

    Remove impermanence link for ssh_hostkeys
    When this link is in place, the impermanence module updates the
    permissions of the keys to 640 which sshd does not accept (for good
    reasons).

--- modules/sshd/default.nix
+++ modules/sshd/default.nix
@@ -8,14 +8,5 @@
       { type = "rsa"; bits = 4096; path = "/persistent/safe/etc/ssh/ssh_host_rsa_key"; }
     ];
   };
-
-  environment.persistence."/persistent/safe" = {
-    files = [
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-    ];
-  };
 }
 
