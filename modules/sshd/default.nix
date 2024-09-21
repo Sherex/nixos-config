fca6e8e7f2495bb864df8aeba34e6e5fb327dfbd
Ingar Helgesen <ingar@i-h.no>
2024-09-21 19:47:15 +0200

    Restrict ssh server

--- modules/sshd/default.nix
+++ modules/sshd/default.nix
@@ -7,6 +7,12 @@
       { type = "ed25519"; path = "/persistent/safe/etc/ssh/ssh_host_ed25519_key"; }
       { type = "rsa"; bits = 4096; path = "/persistent/safe/etc/ssh/ssh_host_rsa_key"; }
     ];
+
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
   };
 }
 
