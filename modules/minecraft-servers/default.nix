645780b7cd293ca0889034813d877bd8f07298f5
Ingar Helgesen <ingar@i-h.no>
2024-11-16 17:21:57 +0100

    Declare the container in Nix

--- modules/minecraft-servers/default.nix
+++ modules/minecraft-servers/default.nix
@@ -1,6 +1,62 @@
 { inputs, config, pkgs, lib,  ... }:
 
 {
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.oci-containers.containers.all-the-mods-9 = {
+    image = "itzg/minecraft-server:java17";
+    # tty = true;
+    # stdin_open = true;
+    # stop_grace_period = "1m";
+    # restart = always;
+    ports = [
+      "25565:25565"
+    ];
+    #workdir = "/srv/containers/all-the-mods-9/";
+    volumes = [
+      # attach the relative directory 'data' to the container's /data path
+      "/srv/containers/all-the-mods-9//container-data:/data:Z"
+      "/srv/containers/all-the-mods-9//downloads:/downloads:Z"
+    ];
+
+    environment = {
+      TZ = "Europe/Oslo";
+
+      EULA = "TRUE";
+      MAX_BUILD_HEIGHT = "1024";
+      SPAWN_PROTECTION = "0";
+      ALLOW_FLIGHT = "true";
+      ENABLE_COMMAND_BLOCK = "true";
+      #ENABLE_RCON = "false";
+
+      #TYPE = "CUSTOM";
+      VERSION = "1.20.1";
+      INIT_MEMORY = "1G";
+      MAX_MEMORY = "15G";
+      USE_AIKAR_FLAGS = "true";
+      TUNE_VIRTUALIZED = "true";
+      #CUSTOM_SERVER = "/data/start.sh";
+      #NEOFORGE_VERSION = "beta";
+
+      #ENABLE_AUTOPAUSE = "FALSE"; # Doesn't work with rootless
+      MAX_TICK_TIME = "-1";
+      #AUTOPAUSE_TIMEOUT_INIT = "30";
+      #AUTOPAUSE_TIMEOUT_EST = "1800";
+
+      DEBUG_AUTOPAUSE = "true";
+      DEBUG_AUTOSTOP = "true";
+
+      RCON_CMDS_LAST_DISCONNECT = ''
+        say Last disconnected
+      '';
+
+      #CF_API_KEY = "";
+      #CF_PAGE_URL = "https://www.curseforge.com/minecraft/modpacks/bm-revelations-2/files/5715118";
+      TYPE = "CURSEFORGE";
+      CF_SERVER_MOD = "/downloads/server-files.zip";
+      CF_DOWNLOADS_REPO = "/downloads";
+    };
+  };
+
   # src: https://nixos.wiki/wiki/Podman
 
   # Enable common container config files in /etc/containers
