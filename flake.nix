199747ac54662fd4ad297aa72b3f088f68ebb7d5
Ingar Helgesen <ingar@i-h.no>
2024-12-25 16:48:11 +0100

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-borgbackup.url = "github:Scrumplex/nixpkgs/nixos/borgbackup/fix-extraArgs";
+    nixpkgs-borgbackup.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
