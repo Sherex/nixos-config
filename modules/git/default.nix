8d382d2821d0973c14edea238ff782457f3c1fb7
Ingar Helgesen <ingar@i-h.no>
2025-03-01 15:57:11 +0100

    Configure git with preffered settings

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -6,6 +6,43 @@
     # TODO: Use variables for name and email
     userName = "Ingar Helgesen";
     userEmail = "ingar@i-h.no";
+    extraConfig = {
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "main";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        all = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      merge = {
+        conflictstyle = "zdiff3";
+      };
+      pull = {
+        rebase = true;
+      };
+    };
   };
 }
 
