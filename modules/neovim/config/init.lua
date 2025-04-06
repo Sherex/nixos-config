f3dba20d93a9bd6de405c95b4dd4b8dd877128ad
Ingar Helgesen <ingar@i-h.no>
2025-04-06 19:45:42 +0200

    Remove unnecessary keybinds for Trouble after Lspsaga

--- modules/neovim/config/init.lua
+++ modules/neovim/config/init.lua
@@ -78,36 +78,6 @@ require("lazy").setup({
     keys = {
       { "<leader>te", "<cmd>TroubleToggle<cr>",          desc = "Toggle Trouble window" },
       { "gr",         "<cmd>Trouble lsp_references<cr>", desc = "Show references for word under cursor" },
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
     },
   },
 
