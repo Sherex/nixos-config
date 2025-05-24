b7006cb84cea22c9dee00867d8d2ee80552f6795
Ingar Helgesen <ingar@i-h.no>
2025-05-24 22:43:05 +0200

    Move XDG portal setup to home-manager to fix conflict of portals

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -30,25 +30,7 @@ in {
     nerd-fonts.hack
   ];
 
-  # xdg-desktop-portal works by exposing a series of D-Bus interfaces
-  # known as portals under a well-known name
-  # (org.freedesktop.portal.Desktop) and object path
-  # (/org/freedesktop/portal/desktop).
-  # The portal interfaces include APIs for file access, opening URIs,
-  # printing and others.
   services.dbus.enable = true;
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    config = {
-      # gtk portal needed to make gtk apps happy
-      common.default = ["gtk" "hyprland"];
-    };
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
-    ];
-  };
 
   # Autostart Hyprland on login
   environment.loginShellInit = ''
@@ -67,6 +49,25 @@ in {
     xdg.configFile."hypr/xdph.conf".text = ''
       allow_token_by_default = true
     '';
+
+    # xdg-desktop-portal works by exposing a series of D-Bus interfaces
+    # known as portals under a well-known name
+    # (org.freedesktop.portal.Desktop) and object path
+    # (/org/freedesktop/portal/desktop).
+    # The portal interfaces include APIs for file access, opening URIs,
+    # printing and others.
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config = {
+        # gtk portal needed to make gtk apps happy
+        common.default = ["gtk" "hyprland"];
+      };
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-hyprland
+      ];
+    };
 
     wayland.windowManager.hyprland = {
       enable = true;
