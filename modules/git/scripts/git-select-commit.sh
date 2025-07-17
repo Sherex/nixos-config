9d06cc43d626cb958125bf33a698080fe7ee4ee1
Ingar Helgesen <ingar@i-h.no>
2025-07-17 19:46:37 +0200

    Add more functionality to git-select-commit

--- modules/git/scripts/git-select-commit.sh
+++ modules/git/scripts/git-select-commit.sh
@@ -2,8 +2,19 @@
 # Can be used like "git diff `gsc`..`gsc`"
 gsc() {
   local selected
-  selected=$(git log --oneline --decorate=short --color=always --date=relative --pretty=format:'%C(auto)%h %C(blue)%ad%Creset %s %d' |
-             fzf --ansi --no-sort --reverse --tiebreak=index --height=40% --prompt="Select commit: ")
+  selected=$(git log --oneline --decorate=short --color=always --date=relative \
+              --pretty=format:'%C(auto)%h %C(blue)%ad%Creset %s %d' |
+    fzf --ansi --no-sort --reverse --tiebreak=index --height=95% \
+        --prompt="Type hash or message to filter: " \
+        --nth=3.. \
+        --preview='
+          commit=$(echo {} | awk "{print \$1}")
+          git show --color=always "$commit"
+        ' \
+        --preview-window=down:70%:wrap:follow \
+        --bind 'ctrl-o:execute-silent(git checkout $(echo {} | awk "{print \$1}"))+abort' \
+        --bind 'ctrl-y:execute-silent(echo {} | awk "{print \$1}" | wl-copy -n)' \
+        --header='Keybinds: ctrl-o=checkout, ctrl-y=copy hash')
 
   [[ -z "$selected" ]] && return 1
 
