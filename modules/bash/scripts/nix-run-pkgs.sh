88e35691927319f40210f61e3636d3da608e87fe
Ingar Helgesen <ingar@i-h.no>
2025-07-17 18:35:03 +0200

    Add help option to nr

--- modules/bash/scripts/nix-run-pkgs.sh
+++ modules/bash/scripts/nix-run-pkgs.sh
@@ -5,12 +5,34 @@ nr() {
   local pkg=""
   OPTIND=1
 
+  show_help() {
+    command cat <<EOF
+Usage: nr [-s] [-d] [-e executable] <package-name> [-- args...]
+
+Options:
+  -s                Run with sudo
+  -e <executable>   Override default executable (default: same as package name)
+  -d                Run in background using 'hyprctl dispatch exec'
+  -h                Show this help message
+
+Arguments:
+  <package-name>    The Nix package to run
+  -- [args...]      Arguments passed to the executable
+
+Examples:
+  nr jq -- .foo.json
+  nr -s e2fsprogs -- mke2fs /dev/sdX
+  nr -d firefox
+EOF
+  }
+
   # Parse options
-  while getopts ":se:d" opt; do
+  while getopts ":se:dh" opt; do
     case $opt in
       s) exec_prefix="sudo" ;;
       e) exec_override="$OPTARG" ;;
       d) exec_background="hyprctl dispatch exec" ;;
+      h) show_help; return 0 ;;
       \?) echo "Invalid option: -$OPTARG" >&2; return 1 ;;
       :) echo "Option -$OPTARG requires an argument." >&2; return 1 ;;
     esac
@@ -18,9 +40,9 @@ nr() {
   shift $((OPTIND -1))
 
   pkg="$1"
-
   if [[ -z "$pkg" ]]; then
-    echo "Usage: nix_run_pkg [-s] [-e executable] <package-name> [-- args...]"
+    echo "Error: Missing <package-name>."
+    show_help
     return 1
   fi
 
