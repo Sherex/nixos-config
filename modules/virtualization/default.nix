3a01c3ad8b233231ceef463a69a17c14cb923768
Ingar Helgesen <ingar@i-h.no>
2025-10-25 18:50:18 +0200

    Add support for TPMv2 in VMs (nixos/nixpkgs#421549)
    This is a temporary workaround until nixos/nixpkgs#421549
    is merged.

--- modules/virtualization/default.nix
+++ modules/virtualization/default.nix
@@ -18,7 +18,13 @@
   virtualisation.libvirtd = {
     enable = true;
     onBoot = "ignore";
-    qemu.swtpm.enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      ovmf.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
+      swtpm.enable = true;
+      runAsRoot = false;
+    };
     extraConfig = ''
       unix_sock_group = "libvirtd";
       unix_sock_rw_perms = "0770";
@@ -32,6 +38,16 @@
       "/var/lib/libvirt"
       "/var/lib/swtpm-localca"
     ];
+  };
+
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
+
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+    };
   };
 }
 
