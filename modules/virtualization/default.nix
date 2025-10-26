1e1dd0d65e4c86467183db7313ecbce9e37ef2fb
Ingar Helgesen <ingar@i-h.no>
2025-10-26 22:48:01 +0100

    Remove deprecated ovmf options

--- modules/virtualization/default.nix
+++ modules/virtualization/default.nix
@@ -20,8 +20,6 @@
     onBoot = "ignore";
     qemu = {
       package = pkgs.qemu_kvm;
-      ovmf.enable = true;
-      ovmf.packages = [ pkgs.OVMFFull.fd ];
       swtpm.enable = true;
       runAsRoot = false;
     };
