98122e1862b98fe55e9435b3eed07019dcdf14ee
Ingar Helgesen <ingar@i-h.no>
2026-03-02 21:34:47 +0100

    Allow public access to SFTPGo shares

--- modules/sftpgo/default.nix
+++ modules/sftpgo/default.nix
@@ -2,29 +2,55 @@
 
 let
   root_domain = "i-h.no";
+  proxyPassDynamic = backend: ''
+    # Set proxy_pass using variable to force runtime DNS lookup
+    # The nixtron hostname is only resolvable when Headscale is
+    # running, which is only reachable through Nginx...
+    # (circular dependency)
+    set $backend "${backend}";
+    proxy_pass $backend;
+
+    proxy_ssl_verify off;
+  '';
+
+  allowOnlyTailnet = ''
+    allow 127.0.0.1;
+    allow 100.70.0.0/16; # Tailscale
+    #allow 1.2.3.4/32; # Useful if devices needs access before Tailscale is setup
+    deny all;
+  '';
 in {
   # The actual SFTPGo service is running on Nixtron for the time being.
-
   services.nginx.virtualHosts."files.${root_domain}" = {
     useACMEHost = "${root_domain}";
     forceSSL = true;
     locations."/" = {
       extraConfig = ''
-        # Set proxy_pass using variable to force runtime DNS lookup
-        # The nixtron hostname is only resolvable when Headscale is
-        # running, which is only reachable through Nginx...
-        # (circular dependency)
-        set $backend "https://nixtron.i.i-h.no:8081";
-        proxy_pass $backend;
-
-        allow 127.0.0.1;
-        allow 100.70.0.0/16; # Tailscale
-        #allow 1.2.3.4/32; # Useful if devices needs access before Tailscale is setup
-        deny all;
-        proxy_ssl_verify off;
+        ${proxyPassDynamic "https://nixtron.i.i-h.no:8080"}
+        ${allowOnlyTailnet}
+        client_max_body_size 0;
+      '';
+    };
+    locations."/web/client/pubshares/" = {
+      extraConfig = ''
+        ${proxyPassDynamic "https://nixtron.i.i-h.no:8080"}
+        allow all;
+        client_max_body_size 0;
+      '';
+    };
+    locations."/static/" = {
+      extraConfig = ''
+        ${proxyPassDynamic "https://nixtron.i.i-h.no:8080"}
+        allow all;
+      '';
+    };
+    locations."/webdav/" = {
+      extraConfig = ''
+        ${proxyPassDynamic "https://nixtron.i.i-h.no:8081"}
+        ${allowOnlyTailnet}
+        client_max_body_size 0;
       '';
     };
   };
-
 }
 
