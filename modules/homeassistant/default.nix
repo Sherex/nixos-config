7813dc6cece823e79715247fe19f5baa84eb5e4d
Ingar Helgesen <ingar@i-h.no>
2026-05-05 09:45:32 +0200

    Add virtual hosts for z2m and esphome + websocket fix

--- modules/homeassistant/default.nix
+++ modules/homeassistant/default.nix
@@ -2,6 +2,16 @@
 
 let
   root_domain = "i-h.no";
+  proxyPassDynamic = backend: ''
+    # Set proxy_pass using variable to force runtime DNS lookup
+    # The nixtron hostname is only resolvable when Headscale is
+    # running, which is only reachable through Nginx...
+    # (circular dependency)
+    set $backend "${backend}";
+    proxy_pass $backend;
+
+    proxy_ssl_verify off;
+  '';
 in {
   # The actual Home Assistant service is running on Nixxer as a container for the time being.
 
@@ -9,25 +19,42 @@ in {
     useACMEHost = "${root_domain}";
     forceSSL = true;
     locations."/" = {
+      proxyWebsockets = true;
+      proxyPass = "http://100.70.0.3:8100";
       extraConfig = ''
-        # Set proxy_pass using variable to force runtime DNS lookup
-        # The nixxer hostname is only resolvable when Headscale is
-        # running, which is only reachable through Nginx...
-        # (circular dependency)
-        set $backend "http://nixxer.i.i-h.no:8100";
-        proxy_pass $backend;
-
         allow 127.0.0.1;
         allow 100.70.0.0/16; # Tailscale
         deny all;
-        proxy_ssl_verify off;
-
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection $connection_upgrade;
       '';
     };
   };
 
+  services.nginx.virtualHosts."zigbee2mqtt.${root_domain}" = {
+    useACMEHost = "${root_domain}";
+    forceSSL = true;
+    locations."/" = {
+      proxyWebsockets = true;
+      proxyPass = "http://100.70.0.3:10201";
+      extraConfig = ''
+        allow 127.0.0.1;
+        allow 100.70.0.0/16; # Tailscale
+        deny all;
+      '';
+    };
+  };
+
+  services.nginx.virtualHosts."esphome.${root_domain}" = {
+    useACMEHost = "${root_domain}";
+    forceSSL = true;
+    locations."/" = {
+      proxyWebsockets = true;
+      proxyPass = "http://100.70.0.3:6052";
+      extraConfig = ''
+        allow 127.0.0.1;
+        allow 100.70.0.0/16; # Tailscale
+        deny all;
+      '';
+    };
+  };
 }
 
