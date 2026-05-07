d8bbcfa817d743f3f735c97ab80bad698d9261bb
Ingar Helgesen <ingar@i-h.no>
2026-05-07 09:00:18 +0200

    replace neodev.nvim with lazydev.nvim

--- modules/neovim/config/init.lua
+++ modules/neovim/config/init.lua
@@ -99,7 +99,17 @@ require("lazy").setup({
 
   { "folke/neoconf.nvim",       cmd = "Neoconf" },
 
-  "folke/neodev.nvim",
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 
   {
     "folke/trouble.nvim",
